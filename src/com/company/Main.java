dba4ea7cbb58fdc75ebc8fb093db8e9b7fa634a9
BT_1N3_13@BKTECH.LOCAL <BT_1N3_13@BKTECH.LOCAL>
2018-10-09 08:38:58 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("What is your name?");
+	String str = input.nextLine();
+	System.out.println("Hello World! My name is " + str);// write your code here
+    }
+}
